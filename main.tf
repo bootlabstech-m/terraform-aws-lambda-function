65beb2513de19eb6ca53c2d01d86242e3f017929
lalita2407 <lalitalallu02@gmail.com>
2024-11-06 11:48:12 +0530

    container based variable

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
      "Action": [
        "logs:CreateLogGroup",
        "logs:CreateLogStream",
-       "logs:PutLogEvents"
+       "logs:PutLogEvents",
        "ecr:GetDownloadUrlForLayer",
        "ecr:BatchGetImage",
        "ecr:DescribeImages"
